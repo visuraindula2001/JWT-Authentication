4036125c265095598b9237c1f4c6701379d9d178
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-11-03 19:54:52 +0530

    Refresh token

--- authServer.js
+++ authServer.js
@@ -4,16 +4,33 @@ const app = express();
 const jwt = require("jsonwebtoken");
 app.use(express.json());
 
+let refreshTokens = []; //Usually store in database
+
+app.post("/token", (req, res) => {
+  const refreshToken = req.body.token;
+  if (refreshToken == null) return res.sendStatus(401);
+  if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403);
+  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, uesr) => {
+    if (err) return res.sendStatus(403);
+    const accessToken = generateAcessToken({name: user.name});
+    res.json({accessToken:accessToken})
+  });
+});
 
 app.post("/login", (req, res) => {
+  //Authenticate user
   const username = req.body.username;
   const user = { name: username };
 
-  const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET); //jwt.sign(payload, secretOrPrivateKey, [options, callback])
-  res.json({ accessToken: accessToken });
+  const accessToken = generateAcessToken(user);
+  const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
+  refreshTokens.push(refreshToken);
+  res.json({ accessToken: accessToken, refreshToken: refreshToken });
 });
 
-
+function generateAcessToken(user) {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "15s" }); //jwt.sign(payload, secretOrPrivateKey, [options, callback])
+}
 
 app.listen(4000, () => {
   console.log("Port is running now");
