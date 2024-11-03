596406c33072a6d9547bb00518277dbe8125ab38
Visura Indula Malawara Arachchi <visuraindula2001@gmail.com>
2024-11-03 19:21:00 +0530

    remove function

--- authServer.js
+++ authServer.js
@@ -13,18 +13,7 @@ app.post("/login", (req, res) => {
   res.json({ accessToken: accessToken });
 });
 
-function authenticateToken(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(' ')[1];
-  if (token == null) return res.sendStatus(401);
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err,user) =>{
-    if(err) return res.sendStatus(403)
-    req.user = user;
-    next();
-  } )
-
-}
 
 app.listen(4000, () => {
   console.log("Port is running now");
